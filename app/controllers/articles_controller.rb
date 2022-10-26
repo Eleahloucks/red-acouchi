c324dfa8db8af90bd42ff9f77265ea808f0be141
eleahmloucks <eleahloucks@gmail.com>
2022-10-26 15:22:06 -0600

    added article to model, added index method to controller and added first route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,10 @@
+#CRUD plus list
 class ArticlesController < ApplicationController
+  #get articles returns a list of article in descending order from date published
+  def index
+    #payload is what my api sends back to the request
+    #should contain my list of articles in desc order
+    payload = []
+    render json: payload, status: :ok
+  end
 end
